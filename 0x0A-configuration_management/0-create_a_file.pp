277fd6791b2654841236102d7a83d9f1b507ff8e
zafrullahidris <108731105+ZafrullahId@users.noreply.github.com>
2024-07-08 01:30:04 +0100

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Create a file in /tmp
 
-file { 'holberton':
-  ensure  => 'present',
-  content => 'I love Puppet',
-  group   => 'www-data',
-  mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/holberton',
-}
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
